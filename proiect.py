774af597b8f78b1dc5daf802c00da25c6aa78103
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-12-16 17:32:02 +0200

    functie adecvare

--- proiect.py
+++ proiect.py
@@ -1,10 +1,26 @@
-def generatePopulation(number, map):
-    population = []
-    initial_cromozom = [index for index in range(len(map[0]))]
-    for chromosome_number in range(number):
-        chromosome_number = []
-        for i in 
+import random
 
+def generatePopulation():
+    population = []
+    initial_cromosome = [index for index in range(number_of_cities)]
+    initial_cromosome.pop(initial_cromosome.index(start_city_index))
+    for chromosome_number in range(chromosomes_number):
+        chromosome = initial_cromosome.copy()
+        random.shuffle(chromosome)
+
+        chromosome.insert(0, start_city_index)
+        chromosome.insert(len(chromosome), start_city_index)
+        print(chromosome)
+
+        population.append(chromosome)
+
+# def generarePopulatie():
+#     for i in range(dimensiunePopulatie):
+#         valoriUniceAleUnuiIndivid = [i for i in range(numarGene)]
+#         individ = valoriUniceAleUnuiIndivid.copy()
+#         random.shuffle(individ)
+#         populatie.append(individ)
+        
 def generateMap():
 # Barcelona, Madrid, Valencia, Bilbao, Seville, Malaga, Zaragoza, San Sebastian, A Coruna, Alicante :
 # Barcelona         : 0, 626, 349, 608, 994, 980, 307, 566, 1083, 528
@@ -17,7 +33,7 @@ def generateMap():
 # SanSeb.           : 566, 457, 573, 101, 918, 983, 264, 0, 639, 746
 # A Coruna          : 1083, 593, 950, 542, 926, 1123, 781, 639, 0, 1071
 # Alicante          : 528, 432, 167, 786, 595, 477, 484, 746, 1071, 0
-    return [[0, 626, 349, 608, 994, 980, 307, 566, 1083, 528]
+    return [[0, 626, 349, 608, 994, 980, 307, 566, 1083, 528],
 [626, 0, 360, 403, 531, 542, 320, 457, 593, 432],
 [349, 360, 0, 612, 655, 623, 309, 573, 950, 167],
 [608, 403, 612, 0, 861, 925, 302, 101, 542, 786],
@@ -28,10 +44,13 @@ def generateMap():
 [1083, 593, 950, 542, 926, 1123, 781, 639, 0, 1071],
 [528, 432, 167, 786, 595, 477, 484, 746, 1071, 0]]
 
+def functieDeCost(): # echivalent cu aia de ne trb la selectie
+
 
 distances = generateMap()
+number_of_cities = len(distances[0])
+start_city_index = 1
 
 chromosomes_number = 100
-population = generatePopulation(chromosomes_number, distances)
-
 genes_number = 10
+population = generatePopulation()
